5390029e77b78ff8c26fd87ab0ba16df07ec705b
Victor A Baez Puente <vbaezp@gmail.com>
2019-04-10 21:15:51 +0200

    Update server.py

--- app/server.py
+++ app/server.py
@@ -10,7 +10,7 @@ from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
 #export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_url = 'https://drive.google.com/uc?export=download&id=1LJ8sICyTJ6m81qGrBGhgAYkGVsrmRTiZ'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1C-ptfuUDjEGqrpDKl6RZbXfDESTBpvw-'
 export_file_name = 'export.pkl'
 
 #classes = ['black', 'grizzly', 'teddys']
